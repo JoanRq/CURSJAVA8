14539a71c3227357f1d361a141274c38828c3319
Joan Roqué <poo2@aula.local>
2016-06-10 13:58:41 +0200

    alguna cosa mes

---
+++ 2016.06.10/Objeto.java
@@ -0,0 +1,13 @@
+interface Nocturno {}
+class N implements Nocturno {}
+
+public class Objeto {
+  public static void main(String[] args) {
+    Nocturno n = null;
+    objeto(n);
+  }
+  
+  public static void objeto(Object o) {
+  
+  }
+}
